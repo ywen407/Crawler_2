b0428a578e873f7180b1f782253f72ad66aa81ba
Iksu Eom <ywen407@naver.com>
2020-06-12 21:37:50 +0900

    modify urlparser

--- src/lib.rs
+++ src/lib.rs
@@ -13,76 +13,77 @@ mod tests {
     use frontier::dns_cashe::DnsCache;
     use frontier::coordinator::Coordinator;
     use agent::agent::Agent;
+    use util::urlparser;
     use util::urlparser::UrlParser;
     #[test]
     fn test_url_parsing() {
 
-        let url = UrlParser::url("ftp://naver.com:20/path/sdf/?query=sdfasdsdfs@34");
+        let url = UrlParser::url("ftp://naver.com:20/path/sdf/?query=sdfasdsdfs@34").unwrap();
         assert_eq!("ftp://naver.com:20/path/sdf/?query=sdfasdsdfs@34",url.get_url());
-        assert_eq!("ftp",url.protocol());
+        assert_eq!(urlparser::Protocol::FTP,url.protocol());
         assert_eq!("naver.com:20",url.hostname());
         assert_eq!("naver.com",url.host());
-        assert_eq!("20",url.port());
+        assert_eq!(20,url.port());
         assert_eq!("/path/sdf/",url.path());
         assert_eq!("query=sdfasdsdfs@34",url.query());  
 
-        let url = UrlParser::url("https://naver.com:80/path/sdf/?query=sdfasdsdfs@34");
+        let url = UrlParser::url("https://naver.com:80/path/sdf/?query=sdfasdsdfs@34").unwrap();
         assert_eq!("https://naver.com:80/path/sdf/?query=sdfasdsdfs@34",url.get_url());
-        assert_eq!("https",url.protocol());
+        assert_eq!(urlparser::Protocol::HTTPS,url.protocol());
         assert_eq!("naver.com:80",url.hostname());
         assert_eq!("naver.com",url.host());
-        assert_eq!("80",url.port());
+        assert_eq!(80,url.port());
         assert_eq!("/path/sdf/",url.path());
         assert_eq!("query=sdfasdsdfs@34",url.query());
         //protocol parsing
-        let url = UrlParser::url("www.naver.com:80/path/sdf/?query=sdfasdsdfs@34");
+        let url = UrlParser::url("www.naver.com:80/path/sdf/?query=sdfasdsdfs@34").unwrap();
         assert_eq!("www.naver.com:80/path/sdf/?query=sdfasdsdfs@34",url.get_url());
-        assert_eq!("https",url.protocol()); //cuz default https
+        assert_eq!(urlparser::Protocol::HTTPS,url.protocol()); //cuz default https
         assert_eq!("www.naver.com:80",url.hostname());
         assert_eq!("www.naver.com",url.host());
-        assert_eq!("80",url.port());
+        assert_eq!(80,url.port());
         assert_eq!("/path/sdf/",url.path());
         assert_eq!("query=sdfasdsdfs@34",url.query());
         //except protocol parsing
 
-        let url = UrlParser::url("https://naver.com/path/sdf/?query=sdfasdsdfs@34");
+        let url = UrlParser::url("https://naver.com/path/sdf/?query=sdfasdsdfs@34").unwrap();
         assert_eq!("https://naver.com/path/sdf/?query=sdfasdsdfs@34",url.get_url());
-        assert_eq!("https",url.protocol());
+        assert_eq!(urlparser::Protocol::HTTPS,url.protocol());
         assert_eq!("naver.com",url.hostname());
         assert_eq!("naver.com",url.host());
-        assert_eq!("443",url.port()); //default port
+        assert_eq!(443,url.port()); //default port
         assert_eq!("/path/sdf/",url.path());
         assert_eq!("query=sdfasdsdfs@34",url.query());
 
         //except port parsing
 
-        let url = UrlParser::url("naver.com/?query=sdfasdsdfs@34");
+        let url = UrlParser::url("naver.com/?query=sdfasdsdfs@34").unwrap();
         assert_eq!("naver.com/?query=sdfasdsdfs@34",url.get_url());
-        assert_eq!("https",url.protocol());
+        assert_eq!(urlparser::Protocol::HTTPS,url.protocol());
         assert_eq!("naver.com",url.hostname());
         assert_eq!("naver.com",url.host());
-        assert_eq!("443",url.port()); //default port
+        assert_eq!(443,url.port()); //default port
         assert_eq!("/",url.path());
         assert_eq!("query=sdfasdsdfs@34",url.query());
         //changed path parsing
         
-        let url = UrlParser::url("naver.com/path/sdrfd/rre/");
+        let url = UrlParser::url("naver.com/path/sdrfd/rre/").unwrap();
         assert_eq!("naver.com/path/sdrfd/rre/",url.get_url());
-        assert_eq!("https",url.protocol());
+        assert_eq!(urlparser::Protocol::HTTPS,url.protocol());
         assert_eq!("naver.com",url.hostname());
         assert_eq!("naver.com",url.host());
-        assert_eq!("443",url.port()); //default port
+        assert_eq!(443,url.port()); //default port
         assert_eq!("/path/sdrfd/rre/",url.path());
         assert_eq!("",url.query());
         
         //query parsing
 
-        let url = UrlParser::url("naver.com/");
+        let url = UrlParser::url("naver.com/").unwrap();
         assert_eq!("naver.com/",url.get_url());
-        assert_eq!("https",url.protocol());
+        assert_eq!(urlparser::Protocol::HTTPS,url.protocol());
         assert_eq!("naver.com",url.hostname());
         assert_eq!("naver.com",url.host());
-        assert_eq!("443",url.port()); //default port
+        assert_eq!(443,url.port()); //default port
         assert_eq!("/",url.path());
         assert_eq!("",url.query());
 
